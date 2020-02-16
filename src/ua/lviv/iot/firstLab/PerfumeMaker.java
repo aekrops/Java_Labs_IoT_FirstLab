dc2ba475fcaf95d5d0e29132a873c315ba064363
Svyatoslav <svyatoslavtrostynski24@gmail.com>
2020-02-16 22:43:46 +0200

    mistake fixed in PerfumeMaker.java

--- src/ua/lviv/iot/firstLab/PerfumeMaker.java
+++ src/ua/lviv/iot/firstLab/PerfumeMaker.java
@@ -9,7 +9,7 @@ public class PerfumeMaker {
 
 		System.out.println(FirstPerfume);
 		System.out.println(SecondPerfume);
-		System.out.println(ThirdPerfume);
+		System.out.println(ThirdPerfume.toString());
 
 		System.out.println();
 
@@ -19,9 +19,15 @@ public class PerfumeMaker {
 		ThirdPerfume.printNumberOfPerfumes();
 
 		Perfume[] Perfumes = new Perfume[4];
-		for (int iterator = 0; iterator < Perfumes.length; iterator++) {
-			Perfumes[iterator] = new Perfume();
+		
+		int numberOfCreatedObjects = 0;
+		int requiredNumberOfObjects = 4;
+		
+		while (numberOfCreatedObjects < requiredNumberOfObjects) {
+			Perfumes[numberOfCreatedObjects] = new Perfume();
+			numberOfCreatedObjects++;
 		}
+		
 		System.out.println();
 
 		for (Perfume perfume : Perfumes) {
